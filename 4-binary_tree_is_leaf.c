97a73d3454f17b2d3daa001712872d124681d03a
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-28 03:04:56 -0800

    adding binary tree is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Check if a given node is a leaf in the binary tree.
+ *
+ * @node:  Pointer to the node to check
+ *
+ * Return:
+ *   1 if the node is a leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node == NULL)
+{
+/* If node is NULL, return 0 */
+return (0);
+}
+/* Check if the node has no left and right children */
+return (node->left == NULL && node->right == NULL);
+}

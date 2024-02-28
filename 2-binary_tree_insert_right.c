d00ec982604133a4861eabbaa11a58965c142ece
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-28 01:10:34 -0800

    adding isert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Insert a new node as the right-child of the given parent node.
+ * binary_tree_insert_right - Insert a new node as the
+ * right-child of the given parent node.
  * @parent: Pointer to the parent node where the new node
  * will be inserted as the right-child
  * @value: Value to be stored in the new node.
  *
- * Return: A pointer to the newly created node, or NULL on failure or if parent is NULL.
+ * Return: A pointer to the newly created node, or
+ * NULL on failure or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -33,7 +35,7 @@ new_node_allo->left = NULL;
 if (parent->right != NULL)
 {
 new_node_allo->right = parent->right;
- new_node_allo->right->parent = new_node_allo;
+new_node_allo->right->parent = new_node_allo;
 }
 else
 {

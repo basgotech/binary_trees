d4aabfb8b820019b0ced181695d75cf306e388d9
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-28 11:31:21 -0800

    adding depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measure the depth nodes in a binary tree
+ * @tree: Pointer to the node to measure the depth pf bnt
+ * Return: The depth of the node.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+size_t dp;
+
+/* Initialize depth to 0 */
+dp = 0;
+
+ /* Traverse up the tree until the root is reached */
+for (; tree != NULL && tree->parent != NULL; dp++)
+{
+tree = tree->parent;
+}
+
+/* Return the calculated depth */
+return (depth);
+}

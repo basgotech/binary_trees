85816c6c4ddc8503dfd4c437bc126e907cc40762
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-28 00:50:01 -0800

    adding fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,11 +3,11 @@
 /**
  * binary_tree_insert_left - Insert a new node as the left-child
  *  of the given parent node.
- * @parent: Pointer to the parent node where the new node 
+ * @parent: Pointer to the parent node where the new node
  *  will be inserted as the left-child
  * @value: Value to be stored in the new node.
  *
- * Return:  A pointer to the newly created node, or NULL on failure 
+ * Return: A pointer to the newly created node, or NULL of failure
  * or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -18,7 +18,7 @@ binary_tree_t *new_node_allo;
 /*  Check if the parent is NULL */
 if (parent == NULL)
 {
-    return (NULL);
+return (NULL);
 }
 
 /* Allocate memory for the new node */
@@ -27,7 +27,7 @@ new_node_allo = malloc(sizeof(binary_tree_t));
 /* Check for memory allocation failure */
 if (new_node_allo == NULL)
 {
-    return (NULL);
+return (NULL);
 }
 
 /* Initialize the new node with the provided values */

826bb6f9068bc0fba75b68660377007cc7cb998b
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-28 03:11:28 -0800

    adding binary tree is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Check if a given node is a root in the binary tree.
+ *
+ * @node: Pointer to the node to check
+ *
+ * Return:
+ *   1 if the node is a root, 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+/* If node is NULL, return 0 */
+if (node == NULL)
+{
+return (0);
+}
+/* Check if the node has no parent */
+return (node->parent == NULL);
+}

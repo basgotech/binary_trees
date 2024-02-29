639f0ab6cd4ba2471d1bf3bc4e2faca645ed19d7
fraolketema <138703893+fraolketema@users.noreply.github.com>
2024-02-29 16:57:38 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,7 @@
  * binary_tree_height - Measure the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
- * Return: The height of the binary tree.
+ * Return: The height of the binary tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {

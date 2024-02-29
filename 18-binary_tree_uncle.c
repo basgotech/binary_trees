b7bafabdbd5f507fcad05eba55bc362c18aa9372
Basliel Tegegn <basgolilo5@gmail.com>
2024-02-29 04:18:44 -0800

    adding

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+        return (NULL);
+
+    binary_tree_t *parent = node->parent;
+    binary_tree_t *grandparent = parent->parent;
+
+
+    if (grandparent->left != NULL && grandparent->right != NULL)
+    {
+        if (grandparent->left == parent)
+            return (grandparent->right);
+        else
+            return (grandparent->left);
+    }
+
+
+    return (NULL);
+}
